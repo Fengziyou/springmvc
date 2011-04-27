024e4a1ddedfd7d79661601afe1a0bb4c251a1a5
sam <mstitop@gmail.com>
2011-04-27 17:59:40 +0800

    测试多视图展示

--- src/main/java/com/esen/web/MainController.java
+++ src/main/java/com/esen/web/MainController.java
@@ -1,13 +1,31 @@
 package com.esen.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.esen.entity.User;
+
 @Controller
-public class MainController {
+public class MainController extends BaseController{
 	@RequestMapping
-	public void init(Model model) {
-		model.addAttribute("msg", new ReportRender());
+	public String init(Model model,HttpServletRequest req,HttpServletResponse response) {
+		List list = new ArrayList();
+		User user = new User();
+		user.setId("1111");
+		list.add(user);
+		list.add(user);
+		model.addAttribute("list", list);
+		model.addAttribute("msg", getText("info.title"));
+		String ctx = req.getContextPath();
+		model.addAttribute("ctx", "<meta id=\"succezbi-context-path\" name=\"succezbi-context-path\" content=\""+ctx+"\">");
+		return "main";
+		
 	}
 }
--- src/main/java/com/esen/web/ajax/SimpleController.java
+++ src/main/java/com/esen/web/ajax/SimpleController.java
@@ -1,12 +1,18 @@
 package com.esen.web.ajax;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.esen.entity.User;
 
 @Controller("ajaxSimple")
+@Scope("session")
 public class SimpleController {
 	public SimpleController() {
 		System.err.println("---------------------------------------");
@@ -14,24 +20,37 @@ public class SimpleController {
 
 	@RequestMapping
 	@ResponseBody
-	public User json() {
+	public List json() {
+		List list = new ArrayList();
 		User user = new User();
 		user.setId("my id");
 		user.setName("my name");
-		return user;
+		list.add(user);
+		list.add(user);
+		return list;
 	}
 
 	@RequestMapping
 	@ResponseBody
-	public User xml() {
+	public List xml(Model model) {
+		List list = new ArrayList();
 		User user = new User();
 		user.setId("my id");
 		user.setName("my name");
-		return user;
+
+		list.add(user);
+		User user2 = new User();
+		user2.setId("my id");
+		user2.setName("my name");
+		list.add(user);
+		model.addAttribute("msg", "task msg");
+		
+		return list;
 	}
 
 	@RequestMapping
 	public User show() {
+		System.err.println(this);
 		User user = new User();
 		user.setId("my id");
 		user.setName("my name");
